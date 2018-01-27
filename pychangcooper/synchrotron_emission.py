a2879ce2cff8dd747b86c4275338a031be3daa7b
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-01-27 19:39:47 +0100

    adding synchrotron emission

---
+++ pychangcooper/synchrotron_emission.py
@@ -0,0 +1,55 @@
+import numpy as np
+from pygsl.testing.sf import synchrotron_1
+
+
+class SynchrotronEmission(object):
+
+    def __init__(self, photon_energies, gamma_grid, B):
+        """
+        :param photon_energies: the photon energies
+        :param gamma_grid: the electron gamma grid
+        :param B: the B-field in units of Gauss
+        """
+
+
+        self._B = B
+        self._photon_energies = photon_energies
+        self._gamma_grid = gamma_grid
+
+        self._n_photon_energies = len(self._photon_energies)
+        self._n_grid_points = len(self._gamma_grid)
+
+
+        self._build_synchrotron_kernel()
+
+    
+    def _build_synchrotron_kernel(self):
+        """
+        pre build the synchrotron kernel for the integration
+        """
+        self._synchrotron_kernel = np.zeros((self._n_photon_energies, self._n_grid_points))
+        Bcritical = 4.14E13 # Gauss
+
+
+        ec = 1.5 * self._B/Bcritical
+        
+        for i, energy in enumerate(self._photon_energies):
+            for j, gamma in enumerate(self._gamma_grid):
+
+                arg = energy  / (ec * gamma * gamma)
+                                
+                self._synchrotron_kernel[i,j] = synchrotron_1(arg)
+                                
+    def compute_spectrum(self, electron_distribution):
+        """
+        
+
+        """
+
+        spectrum = np.zeros_like(self._photon_energies)
+        
+        for i, energy in enumerate(self._photon_energies):
+    
+            spectrum[i] = (self._synchrotron_kernel[i, 1:] * electron_distribution[1:] * (self._gamma_grid[1:] - self._gamma_grid[:-1])).sum()/(2.* energy )
+
+        return spectrum
