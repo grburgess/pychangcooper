dbe53b2decb44ed0a6695924ce839538933d78a1
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-01-28 12:43:26 +0100

    fix import error

--- pychangcooper/scenarios/synchrotron_cooling.py
+++ pychangcooper/scenarios/synchrotron_cooling.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from python_tricks.cmap_cycle import cmap_intervals
 
+from pychangcooper.chang_cooper import ChangCooper
 from pychangcooper.synchrotron_emission import SynchrotronEmission
 from pychangcooper.utils.progress_bar import progress_bar
 from pychangcooper.utils.cmap_intervals import cmap_intervals
