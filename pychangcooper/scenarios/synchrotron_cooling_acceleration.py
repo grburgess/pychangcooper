29b8740a543e50f6488fd5cca76fa780f17f4a28
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-02-02 15:27:58 +0100

    add continuous injection

--- pychangcooper/scenarios/synchrotron_cooling_acceleration.py
+++ pychangcooper/scenarios/synchrotron_cooling_acceleration.py
@@ -4,7 +4,9 @@ from pychangcooper.chang_cooper import ChangCooper, log_grid_generator
 from pychangcooper.scenarios.generic_cooling_acceleration import GenericCoolingAcceleration
 from pychangcooper.scenarios.continuous_powerlaw_injection import ContinuousPowerlawInjection
 from pychangcooper.photons.photon_emitter import PhotonEmitter
+from pychangcooper.scenarios.continuous_powerlaw_injection import ContinuousPowerlawInjection
 from pychangcooper.photons.synchrotron_emission import SynchrotronEmission, synchrotron_cooling_constant, synchrotron_cooling_time
+
 
 
 class SynchrotronCoolingAccelerationComponent(GenericCoolingAcceleration):
@@ -15,7 +17,7 @@ class SynchrotronCoolingAccelerationComponent(GenericCoolingAcceleration):
 
         super(SynchrotronCoolingAccelerationComponent, self).__init__(C0, t_acc, cooling_index=2, acceleration_index=acceleration_index)
 
-
+        
 class SynchCoolAccel_ImpulsivePLInjection(SynchrotronCoolingAccelerationComponent, PhotonEmitter, ChangCooper):
 
         def __init__(self,
@@ -74,6 +76,60 @@ class SynchCoolAccel_ImpulsivePLInjection(SynchrotronCoolingAccelerationComponen
         
             ChangCooper.__init__(self, n_grid_points, max_grid, delta_t,
                                  initial_distribution, store_progress)
+
+            emission_kernel = SynchrotronEmission(self._grid, self._B)
+
+            PhotonEmitter.__init__(self, n_steps, emission_kernel)
+
+
+class SynchCoolAccel_ContinuousPLInjection(SynchrotronCoolingAccelerationComponent, ContinuousPowerlawInjection, PhotonEmitter, ChangCooper):
+
+        def __init__(self,
+                     B=10.,
+                     index=-2.2,
+                     gamma_injection=1E3,
+                     gamma_cool=2E3,
+                     gamma_max=1E5,
+                     t_acc_fraction = 1.,
+                     acceleration_index=2,
+                     n_grid_points=300,
+                     max_grid=1E7,
+                     store_progress=False):
+
+
+
+            self._B = B
+
+            bulk_gamma = 300.
+
+            
+            ratio = gamma_max / gamma_cool
+            
+            n_steps = np.round(ratio)
+            
+            self._gamma_max = gamma_max
+            self._gamma_cool = gamma_cool
+            self._gamma_injection = gamma_injection
+            self._index = index
+
+            ContinuousPowerlawInjection.__init__(self, gamma_injection, gamma_max, index, N=1.)
+            
+            
+            # calculate the cooling time of the highest energy electron
+            # and set the acceleration time to the fraction of that
+
+            cooling_time = synchrotron_cooling_time(B, gamma_max)
+
+            t_acc = cooling_time * t_acc_fraction
+
+            SynchrotronCoolingAccelerationComponent.__init__(self, B, t_acc, acceleration_index)
+
+        
+            delta_t = min(t_acc, cooling_time)
+
+        
+            ChangCooper.__init__(self, n_grid_points, max_grid, delta_t,
+                                 None, store_progress)
 
             emission_kernel = SynchrotronEmission(self._grid, self._B)
 
